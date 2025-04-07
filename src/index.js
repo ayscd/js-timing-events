5a2630d9d4024ae6de11ae5ab9ad1b2fcfcb1996
ayscd <ayeskacsta@gmail.com>
2025-04-07 14:05:25 -0300

    Update 2 -  milliseconds
    - extracting information from new Date to display with set Interval

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-function displayDate(){
+function displayMilli() {
+    let date = new Date;
+
+    let milli = date.getMilliseconds();
+
+    let milliseconds = document.querySelector("#milliseconds");
+    milliseconds.innerHTML = milli;
+}
+
+function displayDate() {
     let date = document.querySelector("#date")
     date.innerHTML = new Date;
 }
 
 setTimeout(displayDate, 5000);
+setInterval(displayMilli, 1);
